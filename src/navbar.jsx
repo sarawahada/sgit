3ec27e9f084f2658dc6f39400e27d26a09ea6b49
Khalil <khalil.chettaoui06@gmail.com>
2021-07-27 09:18:01 +0100

    first commit

---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+export default function navbar() {
+  return (
+    <div className="navbar">
+      <h1>Logo</h1>
+      <ul>
+        <li>ACCEUIL</li>
+        <li>INFORMATIONS</li>
+        <li>A PROPOS</li>
+        <li>CONTACT</li>
+      </ul>
+    </div>
+  );
+}
